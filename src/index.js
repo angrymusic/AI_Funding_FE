10b735c389b04acbad4cb66cd7a9d00006f702d9
chb09876 <chb09876@naver.com>
2022-01-17 13:21:27 +0900

    build3 2022-01-17
    basename 재수정
    process.env.public_url -> "https://ai-funding.github.io/AI_Funding_FE"

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={"https://ai-funding.github.io/AI_Funding_FE"}>
       <GlobalStyle />
       <App />
     </BrowserRouter>
